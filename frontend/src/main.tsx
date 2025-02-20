a2801165bafc708314106ea2f63607808c0a6057
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-20 12:08:28 +0530

    feat: implement routing for login, register, and home pages

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/home' element={<App/>}/>
+        <Route path='/' element={<Navigate to='/home'/>}/>
+      </Routes>
+    </Router>
   </StrictMode>
 );
