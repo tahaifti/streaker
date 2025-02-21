43339bd39b34d84c5731d33f70993dfcaec37954
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-21 15:45:06 +0530

    feat: create index file to export core components for easier imports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,11 @@
+import StreakCounter from './StreakCounter'
+import LongestStreak from './LongestStreak'
+import ActivityForm from './ActivityForm'
+import ActivityList from './ActivityList'
+
+export {
+    StreakCounter,
+    LongestStreak,
+    ActivityForm,
+    ActivityList
+}
