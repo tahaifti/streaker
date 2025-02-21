59cf46926528a0a4be966928f8155f6b31ba5fc7
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-21 15:43:05 +0530

    feat: implement API functions for fetching streaks and activities, and adding activities

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AUTH_BASE_URL = import.meta.env.VITE_LOCAL_AUTH_BASE_URL;
-// const API_BASE_URL = import.meta.env.VITE_LOCAL_API_ACTIVITY_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_LOCAL_API_ACTIVITY_BASE_URL;
 
 const auth_api = axios.create({
     baseURL: AUTH_BASE_URL,
@@ -10,18 +10,18 @@ const auth_api = axios.create({
     }
 });
 
-// const api = axios.create({
-//     baseURL: API_BASE_URL,
-//     headers: {
-//         "Content-type": "application/json"
-//     }
-// });
+const api = axios.create({
+    baseURL: API_BASE_URL,
+    headers: {
+        "Content-type": "application/json"
+    }
+});
 
 const registerUser = async (userData: any) => {
     try {
         const response = await auth_api.post("/register", userData);
         return response.data;
-    } catch (error : any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "Error in registering user");
     }
 };
@@ -30,10 +30,43 @@ const loginUser = async (userData: any) => {
     try {
         const response = await auth_api.post("/login", userData);
         return response.data;
-    } catch (error : any) {
+    } catch (error: any) {
         throw new Error(error.response?.data?.message || "Error in logging in user");
     }
 };
 
+const fetchStreaks = async (token: string) => {
+    const response = await api.get('/streak', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    // console.log(`Streaks: ${response.data.streak}`);
+    return response.data.streak;
+};
 
-export { registerUser, loginUser };
+const fetchActivities = async (token: string) => {
+    const response = await api.get('/activities', {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+    return response.data.activities;
+};
+
+const addActivity = async (token: string, description: string) => {
+    console.log(`Adding activity: ${description}`);
+    const response = await api.post('/activities', {
+        date: new Date().toISOString(),
+        description: description,
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    });
+    console.log(`Activity added:`, response.data);
+    return response;
+}
+
+export { registerUser, loginUser, fetchStreaks, fetchActivities, addActivity };
