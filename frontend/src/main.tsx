a14c23f2345a2aff9b1d2256ccd60798735bc6b9
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-21 15:44:27 +0530

    feat: wrap application in AuthProvider to manage authentication context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,19 @@ import App from './App.tsx';
 import './index.css';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
+import { AuthProvider } from './utils/auth.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/home' element={<App/>}/>
-        <Route path='/' element={<Navigate to='/home'/>}/>
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/home' element={<App />} />
+          <Route path='/' element={<Navigate to='/home' />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   </StrictMode>
 );
