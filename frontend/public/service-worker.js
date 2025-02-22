50dc326d5fde65205620cf5e3f3eea82db3d4658
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-22 15:47:40 +0530

    feat: add favicon assets and service worker for improved PWA support

---
+++ frontend/public/service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'streaker-cache-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/manifest.json',
+    '/favicon.ico'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
