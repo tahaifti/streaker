4979a816e43516c7651dfc924422c1f783ff79d2
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-22 11:02:19 +0530

    feat: add methods to save activity, get all activities, and retrieve current and longest streaks in Activity controller

--- streaker-backend/src/controllers/activity.controller.ts
+++ streaker-backend/src/controllers/activity.controller.ts
@@ -4,11 +4,12 @@ import { ActivityService } from "../services/activity.service";
 export class ActivityController {
     constructor(private activityService: ActivityService) { }
 
+    // method to save activity
     async saveActivity(c: Context) {
         const { id: userId } = c.get('jwtPayload');
         const body = await c.req.json();
         const today = new Date();
-        today.setHours(0, 0, 0, 0);
+        // today.setHours(0, 0, 0, 0);
         // if (new Date(body.date) > today) {
         //     return c.json({ message: 'Cannot add future activities' }, 400);
         // }else
@@ -18,6 +19,8 @@ export class ActivityController {
         const activity = await this.activityService.saveActivity(body.date, body.description, userId);
         return c.json(activity, 201);
     }
+
+    // method to get activities
     async getActivities(c: Context) {
         const { id: userId } = c.get('jwtPayload');
         const days = parseInt(c.req.query('days') || '20');
@@ -25,9 +28,24 @@ export class ActivityController {
         return c.json(activities);
     }
 
+    // method to get all activities
+    async getAllActivities(c: Context) {
+        const { id: userId } = c.get('jwtPayload');
+        const activities = await this.activityService.getAllActivities(userId);
+        return c.json(activities);
+    }
+
+    // method to get current streak
     async getStreak(c: Context) {
         const { id: userId } = c.get('jwtPayload');
         const streak = await this.activityService.getCurrentStreak(userId);
+        return c.json({ streak });
+    }
+
+    // method to get longest streak
+    async getLongestStreak(c: Context) {
+        const { id: userId } = c.get('jwtPayload');
+        const streak = await this.activityService.getLongestStreak(userId);
         return c.json({ streak });
     }
 }
