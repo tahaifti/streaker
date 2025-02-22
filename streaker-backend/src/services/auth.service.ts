00b5c4d0515a3389760c2b56e41f413c99dd2e61
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-22 11:02:36 +0530

    feat: refactor registerUser and verifyUser methods for improved readability and consistency

--- streaker-backend/src/services/auth.service.ts
+++ streaker-backend/src/services/auth.service.ts
@@ -5,51 +5,51 @@ import * as bcrypt from 'bcryptjs';
 export class AuthService {
     constructor(private db: PrismaClient | any) { }
 
-    async registerUser(username: string,
-        name: string,
+    async registerUser(name: string,
+        username: string,
         email: string,
         password: string,
-    ){
+    ) {
         try {
             const hashedPassword = await bcrypt.hash(password, 10);
 
             const user = await this.db.user.create({
-                data : {
-                    username,
+                data: {
                     name,
+                    username,
                     email,
                     password: hashedPassword
                 },
-                select : {
-                    id : true,
-                    username : true,
-                    name : true,
-                    email : true,
-                    createdAt : true,
-                    updatedAt : true
+                select: {
+                    id: true,
+                    username: true,
+                    name: true,
+                    email: true,
+                    createdAt: true,
+                    updatedAt: true
                 }
             });
             return user;
-        }catch(e : any){
-            if(e.code == 'P2002'){
-                throw new HTTPException(409, { message :'Username or email already exists'});
+        } catch (e: any) {
+            if (e.code == 'P2002') {
+                throw new HTTPException(409, { message: 'Username or email already exists' });
             }
             throw e;
         }
     }
 
-    async verifyUser(email : string, password : string){
+    async verifyUser(email: string, password: string) {
         const user = await this.db.user.findUnique({
-            where : { email },
+            where: { email },
         })
 
-        if(!user){
-            throw new HTTPException(404, { message : 'User not found'});
+        if (!user) {
+            throw new HTTPException(404, { message: 'User not found' });
         }
 
         const validPassword = await bcrypt.compare(password, user.password);
-        if(!validPassword) {
-            throw new HTTPException(401, { message : 'Invalid password'});
+        if (!validPassword) {
+            throw new HTTPException(401, { message: 'Invalid password' });
         }
         return user;
     }
