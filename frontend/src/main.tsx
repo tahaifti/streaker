2b7b801138a4f8ab7b5c6c3141f2946bcc4d976c
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-02-22 11:03:54 +0530

    feat: add Landing page route to main application

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,16 +6,17 @@ import './index.css';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
 import { AuthProvider } from './utils/auth.tsx';
+import Landing from './pages/Landing.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path='/' element={<Landing/>} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/home' element={<App />} />
-          <Route path='/' element={<Navigate to='/home' />} />
         </Routes>
       </Router>
     </AuthProvider>
