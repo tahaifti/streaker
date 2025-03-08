35773c1279d55b436a61aa8a377183d4014a831d
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-03-08 16:57:22 +0530

    feat: add input validation for user registration and login using Zod schemas

--- streaker-backend/src/controllers/auth.controller.ts
+++ streaker-backend/src/controllers/auth.controller.ts
@@ -2,14 +2,16 @@ import { PrismaClient } from "@prisma/client"
 import { AuthService } from "../services/auth.service"
 import { Context } from "hono"
 import { sign } from "hono/jwt";
+import { createUserSchema, loginSchema } from "@ifti_taha/streaker-common";
 
 export class AuthController{
     constructor(private authService : AuthService) {}
 
     async registerUser(c : Context) {
         const body = await c.req.json();
-        if(!body.email || !body.password) {
-            return c.json({message: 'Email and password are required'}, 400)
+        const { success } = createUserSchema.safeParse(body);
+        if(!success) {
+            return c.json({message: 'Invalid input'}, 400)
         }
         const user = await this.authService.registerUser(body.name, body.username, body.email, body.password);
         return c.json(user, 201);
@@ -17,8 +19,10 @@ export class AuthController{
 
     async loginUser(c : Context){
         const body = await c.req.json();
-        if(!body.email || !body.password) {
-            return c.json({message: 'Email and password are required'}, 400)
+
+        const { success } = loginSchema.safeParse(body);
+        if(!success) {
+            return c.json({message: 'Invalid input'}, 400)
         }
         const user = await this.authService.verifyUser(body.email, body.password);
         const token = await sign({id : user.id}, c.env.JWT_SECRET)
