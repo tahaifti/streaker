d9c662418200e9ccd04e7c8413cb14d71f2ff1b1
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2025-03-09 10:54:59 +0530

    feat: update password validation to require a minimum of 6 characters

--- common/src/index.ts
+++ common/src/index.ts
@@ -13,7 +13,7 @@ export const userSchema = z.object({
         }),
     email: z.string().email({ message: 'Please enter a valid email address' }),
     password: z.string()
-        .min(8, { message: 'Password must be at least 8 characters long' })
+        .min(6, { message: 'Password must be at least 6 characters long' })
         .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
         .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
         .regex(/[0-9]/, { message: 'Password must contain at least one number' }),
